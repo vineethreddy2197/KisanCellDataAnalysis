bffe64278c11adf82a42132c9b6b2a3c953fd98e
vineethreddy2197 <vineethreddy2197@gmail.com>
2018-04-15 14:48:26 +0530

    brush up

--- mymain.py
+++ mymain.py
@@ -3,6 +3,9 @@ print('1->Crop Analysis')
 print('2->Season Analysis')
 print('3->Statewise Analysis')
 print('4->Each State Analysis')
+print('5->Each Category Analysis')
+print('6->Each Query Analysis')
+print('7->Each State Analysis')
 x=int(input('Enter : '))
 if x==1:
     os.system('python crop.py')
@@ -10,5 +13,11 @@ elif x==2:
     os.system('python season.py')
 elif x==3:
     os.system('python statewise.py')
+elif:
+    os.system('python state.py')
+elif:
+    os.system('python category.py')
+elif:
+    os.system('python query.py')
 else:
     os.system('python state.py')
